0bef2fdee8f308caace0e2aa1f0e65a57e2baa1d
Daniel Ferreira <daniel.ferreira00640317@sesisenaipr.org.br>
2023-03-04 20:50:33 -0300

    Adicionar arquivos de projeto.

---
+++ ProjetoSenai/Class1.cs
@@ -0,0 +1,57 @@
+﻿using MySql.Data.MySqlClient;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Runtime.Remoting.Metadata.W3cXsd2001;
+using System.Text;
+using System.Threading.Tasks;
+using WindowsFormsApp3;
+
+namespace ProjetoSenai
+{
+    public class Cadastro : conexao
+    {
+        public Cadastro()
+        {
+            string sla = "sla";
+        }
+
+
+
+
+        public string Cadastrar(string tabela, string argumentos,string adicionar)
+        {
+            try
+            {
+                
+                conexao con = new conexao();
+                MySqlConnection conexao = con.getconexao();
+                conexao.Open();
+                string inserir = "insert into @tabela (@argumentos) values " + "(@adicionar)";
+                MySqlCommand comando = new MySqlCommand(inserir, conexao);
+                comando.Parameters.AddWithValue("@tabela", tabela);
+                comando.Parameters.AddWithValue("@argumentos", argumentos);
+                comando.Parameters.AddWithValue("@adicionar", adicionar); 
+                comando.CommandType = CommandType.Text;
+                comando.ExecuteReader();
+                conexao.Close();
+                //comando.Parameters.AddWithValue("@horario_evento", Convert.ToInt32(horario_evento.Text));
+                //comando.Parameters.AddWithValue("@descricao_evento", descricao_evento.Text);
+                
+               
+                //MessageBox.Show("Foi");
+                
+                return "sem erro";
+                
+            }
+            catch(Exception ex)
+            {
+                string erro = ex.ToString();
+                return erro;
+            }
+            
+        }
+    }
+}
+
---
+++ ProjetoSenai/FormsAdm/Tela_Cadas_Evento.cs
@@ -0,0 +1,45 @@
+﻿using MySql.Data.MySqlClient;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using WindowsFormsApp3;
+
+namespace ProjetoSenai.FormsAdm
+{
+    public partial class Tela_Cadas_Evento : Form
+    {
+        conexao con = new conexao();
+        public Tela_Cadas_Evento()
+        {
+            InitializeComponent();
+        }
+
+        private void cadas_sala_Click(object sender, EventArgs e)
+        {
+
+           
+
+            string argumentos  = "nome_evento,local_evento,data_evento,horario_evento,descricao_evento";
+            string adicionar = "nicolas, nicolas1, 20230412, 131718, nicolas5";
+            Cadastro sla = new Cadastro();
+            string erro = sla.Cadastrar("tb_evento", argumentos,adicionar);
+            MessageBox.Show(erro);
+           
+
+
+
+
+
+
+
+
+
+        }
+    }
+}
